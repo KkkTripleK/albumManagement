37272536e1448c54b9a2467df8240b730857e9d7
HoaNK <hoank@MacBook-Air-cua-HoaNK.local>
2022-07-30 09:58:23 +0700

    update code

--- src/modules/authentication/auth.service.js
+++ src/modules/authentication/auth.service.js
@@ -1,11 +1,27 @@
 // Kiểm tra xem user và pass đã đúng chưa --> call user service
 // Cung cấp token để đăng nhập nếu như đã nhập đúng --> JWT
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 const userService = require('../users/user.service');
+const userRepo = require('../users/user.repository');
 
 async function userLogin(userName, password) {
     console.log('_Stage: auth Service');
     const result = await userService.checkExistAcc(userName, password);
-    console.log(result);
+    if (result) {
+        jwt.verify(token, privateKey, (err, decoded) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+        // const privateKey = 'thismsg';
+        // const token = jwt.sign({ userName }, privateKey, { expiresIn: '5s' });
+        // userRepo.addTokenForUser(userName, token);
+
+        // console.log('Login successful!');
+    } else {
+        console.log('Invalid username or password!');
+    }
 }
+
 module.exports = { userLogin };
